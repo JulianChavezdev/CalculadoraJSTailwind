9372327fc5443fbd9aacdb627d0d55c0e226f1b6
JulianChavezdev <julicgavez@hotmail.com>
2026-06-10 21:16:17 +0200

    adding calculator brain on main.js

---
+++ src/main.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const display = document.getElementById('display');
+    const buttons = document.querySelectorAll('button');
+
+    let actualOp = "";
+    let displayRestart = false;
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const value = button.getAttribute('data-val');
+            inputValue(value);
+        });
+    });
+
+function inputValue(value) {
+
+    if (value === "c" || value === "C" || value === "clear") {
+        actualOp = "";
+        display.textContent = 0;
+        displayRestart = false;
+        return;
+    } 
+    
+
+    if (value === "=" || value === "equal") {
+        try {
+            let expresion = actualOp.replace(/x/g, "*").replace(/,/g, ".");
+            if (expresion === "") return;
+
+            let res = new Function("return " + expresion)();
+        
+            actualOp = String(res);
+            display.textContent = actualOp;
+            displayRestart = true; 
+        } catch (error) {
+            display.textContent = "Error";
+            actualOp = "";
+            displayRestart = true;
+        }
+        return; 
+    }
+
+
+    if (displayRestart) {
+        actualOp = "";
+        displayRestart = false;
+    }
+
+    if (actualOp === "" && value !== "." && !isNaN(value)) {
+        actualOp = value;
+    } else {
+        actualOp += value;
+    }
+    display.textContent = actualOp;
+}
+});
