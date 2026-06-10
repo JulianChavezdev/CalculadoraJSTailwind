110b8d22ad4df8f47b2af8a03cdd01ed752c81c3
JulianChavezdev <julicgavez@hotmail.com>
2026-06-10 22:59:00 +0200

    adding gsap uses

--- src/main.js
+++ src/main.js
@@ -6,13 +6,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const display = document.getElementById('display');
     const buttons = document.querySelectorAll('button');
     const title = document.getElementById('title');
+    const box = document.getElementById('box');
+
 
     let actualOp = "";
     let displayRestart = false;
 
     buttons.forEach(button => {
-        button.addEventListener('click', () => {
+        button.addEventListener('click', (e) => {
             const value = button.getAttribute('data-val');
+            const elementbutton= e.currentTarget;
+
+            let tl = gsap.timeline();
+            tl.to(elementbutton,{scaleY: 0.8, scaleX: 1.2, duration: 0.05, ease: "power1.out"})
+            tl.to(elementbutton, { scaleY: 1.1, scaleX: 0.9, duration: 0.1, ease: "power1.inOut" });
+            tl.to(elementbutton, { scaleY: 1, scaleX: 1, duration: 0.4, ease: "elastic.out(1, 0.3)" });
+
             inputValue(value);
         });
     });
@@ -33,6 +42,15 @@ document.addEventListener('DOMContentLoaded', () => {
         y: 30,
         immediateRender: true
     })
+
+    gsap.from(box, {
+        opacity: 0,
+        duration: 0.3,
+        ease: 'powerinOut',
+        y: 30,
+        immediateRender:true
+    })
+
 
 
 function inputValue(value) {
